963cd360aab56a8a219f1c65df2d8f20e94b45e7
Katarzyna Konieczny <katia_k@op.pl>
2019-05-24 23:29:21 +0200

    Trip Inspiration 24.05.2019 v6

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Map from "./Map";
 
 
 class CountryInfo extends React.Component {
@@ -32,38 +33,21 @@ class CountryInfo extends React.Component {
                 })
     };
 
-    // componentDidMount() {
-    //     fetch(`http://battuta.medunes.net/api/country/search/?country=${this.props.countryName}&key=ef105569401a632963968018de6f0060`)
-    //         .then(resp=>{
-    //             if(resp.ok) {
-    //                 return resp.json()
-    //             }
-    //             // else{
-    //             //     throw new Error("Incorrect country")
-    //             //     }
-    //         })
-    //         .then (json => {
-    //             console.log(json);
-    //
-    //             // this.setState({
-    //             //     country: json
-    //             // });
-    //
-    //             return json
-    //
-    //         })
-    // };
+   
 
     render() {
+
         return (
             <div>
                 <h3>Capital of {this.props.countryName} is {[...this.state.country].map(e => e.capital)}</h3>
-                <img alt="flag" src={[...this.state.country].map(e => e.flag)} height="auto" width="auto" border="2px solid black"/>
+                <img alt="flag" src={[...this.state.country].map(e => e.flag)} height="30%" width="30%" border="2px solid black"/>
                 <p>Area of {this.props.countryName} is {[...this.state.country].map(e => e.area)}</p>
                 <p>In {this.props.countryName} lives {[...this.state.country].map(e => e.population)} people</p>
                 <p>{this.props.countryName} lies in {[...this.state.country].map(e => e.subregion)}</p>
                 <p>The official language in {this.props.countryName} is {[...this.state.country].map(e => e.languages[0].name)} ({[...this.state.country].map(e => e.languages[0].nativeName)})</p>
                 <p>The currency of {this.props.countryName} is {[...this.state.country].map(e => e.currencies[0].name)} ({[...this.state.country].map(e => e.currencies[0].code)})</p>
+
+                <Map country={this.props.countryName} />
             </div>
         );
     }
--- src/components/TripInspirationHeader.js
+++ src/components/TripInspirationHeader.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import TripInspirtionButtons from "./TripInspirationButtons";
 
+import photo1 from "../images/adult-back-view-buddha-750895.jpg";
+import photo2 from "../images/adventure-daylight-desert-320555.jpg";
+import photo3 from "../images/ancient-architect-architecture-2104882.jpg";
+import photo4 from "../images/asia-china-himalayas-1531660.jpg";
+import photo5 from "../images/ancient-architecture-art-732895.jpg";
+import photo6 from "../images/NEPAL_20181004_154110.jpg";
 
 
 function TripInspirationHeader () {
@@ -14,7 +20,12 @@ function TripInspirationHeader () {
             <h2>We will help you choose where you want to go...</h2>
             <TripInspirtionButtons/>
             <div>
-                <img src="../images/ancient-architect-architecture-2104882.jpg" alt='nepal' width="200px" height="100px"/>
+                <img src={photo1} alt="budda" height="30%" width="30%"/>
+                <img src={photo2} alt="desert" height="30%" width="30%"/>
+                <img src={photo3} alt="architecture" height="30%" width="30%"/>
+                <img src={photo4} alt="himalayas" height="30%" width="30%"/>
+                <img src={photo5} alt="architecture" height="30%" width="30%"/>
+                <img src={photo6} alt="Nepal1" height="30%" width="30%"/>
             </div>
 
         </div>
